a46632a1839096cc314a9ab202f757ef3bd7cd41
Maria Jose Osorio <majoosorio2402@gmail.com>
2023-04-28 12:08:54 -0500

    Update Players And gems

--- Assets/Scripts/majoScripts/UIManage.cs
+++ Assets/Scripts/majoScripts/UIManage.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject gemaPlayer1;
+    [SerializeField] GameObject gemaPlayer2;
+    [SerializeField] GameObject gemaPlayer3;
+
+    [SerializeField] GameObject gemaEnemy1;
+    [SerializeField] GameObject gemaEnemy2;
+    [SerializeField] GameObject gemaEnemy3;
+
+
+    [SerializeField] Button buttonPlayer, buttonEnemy;
+  
+    public void Point()
     {
-        
+        gemaPlayer1.SetActive(true);
+        gemaPlayer2.SetActive(true);
+        gemaPlayer3.SetActive(true);
+
+
+
+    }
+    public void Point2()
+    {
+        gemaEnemy1.SetActive(true);
+        gemaEnemy2.SetActive(true);
+        gemaEnemy3.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
