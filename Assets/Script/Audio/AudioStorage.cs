eec94b57e195bf3531cb867476ea66be763fe5a7
matnaranjo <matnaranjo2020@gmail.com>
2023-04-28 09:37:41 -0500

    Added AudioStorage script and object with instructions

---
+++ Assets/Script/Audio/AudioStorage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu (fileName ="AudioStorage", menuName = "cards/AudioStorage", order =0)]
+public class AudioStorage : ScriptableObject
+{
+    // 1 - Crear AudioClips publicos o serialized fields individuales o un array, no importa.
+    // 2 - 
+}
