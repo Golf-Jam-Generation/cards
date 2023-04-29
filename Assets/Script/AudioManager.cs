157d6b00e42c3f189e23cac1a0332086a5e2c46c
JisusTQ <jesus.torresq@udea.edu.co>
2023-04-29 00:52:47 -0500

    Continue with audioscrpts

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -9,10 +9,20 @@ public class AudioManager : MonoBehaviour
     [Header("--------- Audio Clip ------------")]
     public AudioClip gameplay;
     public AudioClip menu;
+    public AudioClip lose;
+    public AudioClip victory;
+    public AudioClip pcPoint;
+    public AudioClip playerPoint;
+    public AudioClip click;
 
     private void Start()
     {
         musicSource.clip = menu;
         musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
     }
 }
